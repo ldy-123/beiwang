f5f06de00b7419168aa85d89aa0c564882f86df8
ldy-123 <ldy_063@163.com>
2026-05-10 17:58:33 +0800

    chore: SW v39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'memo-v38';
+const CACHE = 'memo-v39';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
