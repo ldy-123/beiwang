d7b00f5bd734ce5c9ce99c07716fb49c0fc5be04
ldy-123 <ldy_063@163.com>
2026-05-18 21:05:01 +0800

    chore: bump SW cache version to memo-v49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'memo-v48';
+const CACHE = 'memo-v49';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
